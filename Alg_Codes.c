42521f1be1f55542fb3a1b7c3bb101eb4989ec81
Maykon <maaykon51@gmail.com>
2021-01-25 22:40:22 -0300

    Adding insertion sort algorithm and analysis, also the gitignore for the tests.

--- Alg_Codes.c
+++ Alg_Codes.c
@@ -79,6 +79,22 @@ void quick_sort(int *a, int left, int right) {
     }
 }
 
+
+void insertionSort(int array[], int size) {
+    for (int step = 1; step < size; step++) {
+        int key = array[step];
+        int j = step - 1;
+        // Compara a key com cada elemento a sua esquerda até encontrar onde deve ser inserido ou qual elemento é maior que a chave
+        // Para ondem decrescente,altere key<array[j] para key>array[j].
+        while (key < array[j] && j >= 0) {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+}
+
+
 int main(void) {
 
 //srand(time(NULL)); talvez nao sera preciso
@@ -91,7 +107,8 @@ int main(void) {
     creategrowvector(growvector);
     createdecrevector(decrevector);
     showvector(randvector);
-    quick_sort(randvector,0,MAX-1);
+    //quick_sort(randvector,0,MAX-1);
+    //insertionSort(randvector,MAX);
     showvector(growvector);
     showvector(decrevector);
     showvector(randvector);
